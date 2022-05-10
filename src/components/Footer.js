04cbc11af6c9158a8cdb1cfb2088003e95bd23b4
braum852 <julian.teo59@yahoo.com>
2022-05-10 18:57:14 +1000

    Home Icoon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import "./Footer.css"
-
+import { FaHome } from "react-icons/fa"
 import React from 'react'
 
 const Footer = () => {
   return (
     <div className="footer">
         <div className="footer-container">
-
+            <div className="left">
+                <div className="location">
+                    <FaHome />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     </div>
   )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import BackImg from '../components/BackImg'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <BackImg />
+      <Footer />
     </div>
   )
 }
