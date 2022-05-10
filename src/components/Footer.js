5a17ab8063e009507e0fc263f32b6c1363639160
braum852 <julian.teo59@yahoo.com>
2022-05-10 18:53:32 +1000

    Begin footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
