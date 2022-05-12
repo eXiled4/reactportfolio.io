fe1b6ae84a0ce55912a12861cb110f08622c0683
braum852 <julian.teo59@yahoo.com>
2022-05-12 15:13:38 +1000

    Footer progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import "./Footer.css"
-import { FaHome } from "react-icons/fa"
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa"
 import React from 'react'
 
 const Footer = () => {
@@ -8,7 +8,20 @@ const Footer = () => {
         <div className="footer-container">
             <div className="left">
                 <div className="location">
-                    <FaHome />
+                    <FaHome size={25} style={{color: "white", marginRight: "2rem"}}/>
+                    <div>
+                      <p>Homview, 4207, Queensland</p>
+                      <p>Australia</p>
+                    </div>
+                </div>
+                <div className="phone">
+                  <h4><FaPhone size={25} style={{color: "white", marginRight: "2rem"}}/>
+                  0468 550 898</h4>                  
+                </div>
+
+                <div className="email">
+                  <h4><FaMailBulk size={25} style={{color: "white", marginRight: "2rem"}}/>
+                  julian.teo59@yahoo.com</h4>                  
                 </div>
             </div>
             <div className="right"></div>
