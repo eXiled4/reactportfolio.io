94c93d2affe79e5a4d80831947d295a84518c3b5
braum852 <julian.teo59@yahoo.com>
2022-05-13 22:13:40 +1000

    First Project card progress

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -10,13 +10,13 @@ const WorkCard = () => {
         <div className="project-container">
             <div className="project-card">
                 <img src={trav} alt="reactrailsproj" />
-                <h2 className="project-title">Project Title</h2>
+                <h4 className="project-title">Traveller (A React Rails Project)</h4>
                 <div className="project-details">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <div className="project-btns">
-                            <NavLink to="https://github.com/eXiled4?tab=repositories" className="btn">View</NavLink>
-                            <NavLink to="https://github.com/eXiled4?tab=repositories" className="btn">Source</NavLink>
+                            <NavLink to={{pathname: "https://github.com/eXiled4?tab=repositories"}} className="btn">View</NavLink>
+                            <NavLink to="github.com/eXiled4?tab=repositories" className="btn">Source</NavLink>
                         </div>
                 </div>
             </div>
