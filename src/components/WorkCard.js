607cbdc65b1fbb9b3b3c52b5973d55719fb27df7
braum852 <julian.teo59@yahoo.com>
2022-05-13 21:18:30 +1000

    Recycled css from Pricing Card for Project Card -sizing about right and page responsive

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,11 +1,12 @@
 import "./WorkCard.css";
 import React from 'react'
 import trav from "../assets/Traveller.jpg"
+import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
     <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
+      <h1 className="project-heading">Projects</h1>
         <div className="project-container">
             <div className="project-card">
                 <img src={trav} alt="reactrailsproj" />
@@ -13,6 +14,10 @@ const WorkCard = () => {
                 <div className="project-details">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className="project-btns">
+                            <NavLink to="https://github.com/eXiled4?tab=repositories" className="btn">View</NavLink>
+                            <NavLink to="https://github.com/eXiled4?tab=repositories" className="btn">Source</NavLink>
+                        </div>
                 </div>
             </div>
         </div>
